aee998ba1d40626306bc41d00bbeceb6944c668e
bh1990 <56873720+bh1990@users.noreply.github.com>
2019-10-29 21:03:57 +0700

    Add files via upload

---
+++ break/src/pkgbreak/Break.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkgbreak;
+
+/**
+ *
+ * @author user
+ */
+public class Break {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        for (int i = 0; i < 10; i++){
+            if (i == 4){
+                break;
+            }
+            System.out.println(i);
+        }
+    }
+    
+}
